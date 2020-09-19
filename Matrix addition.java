25b8db64637eca46e64734da0acdd83e5d885903
Divyasri123-svg <70933647+Divyasri123-svg@users.noreply.github.com>
2020-09-19 20:50:18 +0530

    Matrix addition.java
    The file has addition of two matrices in java

---
+++ Matrix addition.java
@@ -0,0 +1,16 @@
+Public class matrixaddition {
+ Public static void main (String[] args)
+{
+int a[][]={{1,3,4},{2,4,3},{3,4,5}};
+int b[][]={{1,3,4},{2,4,3},{1,2,4}};
+int c[][]=new int[i][j];
+for(i=0;i<3;i++){
+  for(j=0;j<3:j++){
+   c[i][j]=a[i][j]+b[i][j];
+      System.out.print(c[i][j]+" ");
+}
+System.out.println();
+}
+}
+}
+
